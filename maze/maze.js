3e5709ed8691c742ca4aed18c0f9ef7626af2ab8
minalubw <minalubiresaw@gmail.com>
2022-12-13 23:31:15 -0600

    w3d2

---
+++ maze/maze.js
@@ -0,0 +1,30 @@
+$(()=>{
+    $("#start").click(function(){
+        $("#status").text("Game Started!");
+        $(".example").text("0%");
+    });
+    
+    $(".boundary").mouseenter(function(){
+        if( $("#status").text().indexOf("Started!") > 0){
+            $("#status").text("You lost! Click S to start again.");
+            alert("You Lost!");
+        }
+    });
+
+    $("#end").mouseenter(function(){
+        if( $("#status").text().indexOf("Started!") > 0){
+            $("#status").text("You won! Click S to start again.");
+            $(".example").text("100%");
+            alert("Congratulations. You Won!");
+           
+        }
+    });
+
+    $("#maze").mouseleave(function(){
+        if( $("#status").text().indexOf("Started!") > 0){
+            $("#status").text("You lost! Click S to start again.");
+            alert("You Lost!");
+        }
+    });
+});
+ 
